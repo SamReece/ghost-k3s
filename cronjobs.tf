ef299bb6cbd3a87edbfddf3dfd511e46c71c306e
Sam R <samreece@protonmail.com>
2026-04-06 14:27:59 +0100

    feat: cronjob

---
+++ cronjobs.tf
@@ -0,0 +1,70 @@
+resource "kubernetes_cron_job" "ghost_mysql_backup" {
+  metadata {
+    name      = "ghost-mysql-backup"
+    namespace = "default"
+  }
+
+  spec {
+    schedule                      = var.backup_schedule
+    successful_jobs_history_limit = 3
+    failed_jobs_history_limit     = 1
+
+    job_template {
+      metadata {}
+      spec {
+        template {
+          metadata {}
+          spec {
+            restart_policy = "OnFailure"
+
+            container {
+              name  = "mysql-backup"
+              image = "ubuntu:22.04" # We'll install mysql-client & rclone
+
+              env_from {
+                secret_ref {
+                  name = kubernetes_secret_v1.mysql_backup.metadata[0].name
+                }
+              }
+
+              volume_mount {
+                name       = "backup-storage"
+                mount_path = "/backup"
+              }
+
+              volume_mount {
+                name       = "rclone-config"
+                mount_path = "/root/.config/rclone"
+              }
+
+              command = ["/bin/sh", "-c"]
+              args = [<<-EOF
+                apt-get update && \
+                apt-get install -y mysql-client rclone gzip && \
+                TIMESTAMP=$(date +%F_%H-%M-%S) && \
+                BACKUP_FILE="/backup/ghost_$TIMESTAMP.sql.gz" && \
+                echo "Dumping database..." && \
+                mysqldump -h ${var.mysql_host} -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE | gzip > $BACKUP_FILE && \
+                echo "Uploading to Contabo..." && \
+                rclone copy $BACKUP_FILE contabo:${var.contabo_bucket}/
+              EOF
+              ]
+            }
+
+            volume {
+              name = "backup-storage"
+              empty_dir {}
+            }
+
+            volume {
+              name = "rclone-config"
+              secret {
+                secret_name = kubernetes_secret_v1.rclone_config.metadata[0].name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -15,3 +15,33 @@ variable "mysql_database" {
   type        = string
   default     = "ghost"
 }
+
+variable "mysql_host" {
+  type        = string
+  default     = "mysql-service"
+}
+
+variable "contabo_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "contabo_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "contabo_endpoint" {
+  type    = string
+  default = "https://eu2.contabostorage.com"
+}
+
+variable "contabo_bucket" {
+  type    = string
+  default = "ghost-backups"
+}
+
+variable "backup_schedule" {
+  type    = string
+  default = "0 2 * * *" # daily at 2 AM
+}
